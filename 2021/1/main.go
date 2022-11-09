04f51f2112096844e7e15e9e80b4702a0a81f19d
Kurt McAlpine <kurt@linux.com>
2022-11-09 14:25:52 +1300

    day 1, part 2

--- 2021/1/main.go
+++ 2021/1/main.go
@@ -8,17 +8,60 @@ import (
 	"strconv"
 )
 
+// part 1
+// func main() {
+// 	file, err := os.Open("input.txt")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer file.Close()
+//
+// 	previous := -1
+// 	increasesCount := 0
+//
+// 	scanner := bufio.NewScanner(file)
+// 	for scanner.Scan() {
+// 		text := scanner.Text()
+// 		if text == "" {
+// 			break
+// 		}
+//
+// 		i, err := strconv.Atoi(text)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+//
+// 		if previous != -1 {
+// 			if i > previous {
+// 				increasesCount++
+// 			}
+// 		}
+//
+// 		previous = i
+// 	}
+//
+// 	if err := scanner.Err(); err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+// 	fmt.Printf("count: %d\n", increasesCount)
+// }
+
+// part 2
 func main() {
-	file, err := os.Open("input.txt")
+	file, err := os.Open("input-part-2.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	previous := -1
+	slidingWindowSize := 3
+	sw := NewSlidingWindow(slidingWindowSize)
+
 	increasesCount := 0
 
 	scanner := bufio.NewScanner(file)
+	index := 0
 	for scanner.Scan() {
 		text := scanner.Text()
 		if text == "" {
@@ -30,13 +73,18 @@ func main() {
 			panic(err)
 		}
 
-		if previous != -1 {
-			if i > previous {
-				increasesCount++
-			}
+		previousSwSum := sw.Sum()
+
+		sw.Put(i)
+
+		swSum := sw.Sum()
+
+		if index >= slidingWindowSize && swSum > previousSwSum {
+			increasesCount++
 		}
 
-		previous = i
+		index++
+
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -44,4 +92,35 @@ func main() {
 	}
 
 	fmt.Printf("count: %d\n", increasesCount)
+}
+
+type SlidingWindow struct {
+	size     int
+	index    int
+	elements []int
+}
+
+func NewSlidingWindow(size int) *SlidingWindow {
+	return &SlidingWindow{
+		size:     size,
+		index:    0,
+		elements: []int{},
+	}
+}
+
+func (s *SlidingWindow) Put(x int) {
+	if s.index < s.size {
+		s.elements = append(s.elements, x)
+	} else {
+		s.elements[s.index%s.size] = x
+	}
+	s.index++
+}
+
+func (s *SlidingWindow) Sum() int {
+	sum := 0
+	for _, v := range s.elements {
+		sum = sum + v
+	}
+	return sum
 }
