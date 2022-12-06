11cd87011042b3b8f84c33da94d393487e30d0ea
Kurt McAlpine <kurt@linux.com>
2022-12-07 10:52:25 +1300

    Part 2

--- 2022/6/main_test.go
+++ 2022/6/main_test.go
@@ -5,7 +5,6 @@ import (
 )
 
 func TestStartOfPacket(t *testing.T) {
-
 	tests := []struct {
 		input string
 		want  int
@@ -30,6 +29,41 @@ func TestStartOfPacket(t *testing.T) {
 
 	for i, tc := range tests {
 		got := StartOfPacket(tc.input)
+		if got != tc.want {
+			t.Fatalf("[%d]: expected: %v, got: %v", i, tc.want, got)
+		}
+	}
+}
+
+func TestStartOfMessage(t *testing.T) {
+	tests := []struct {
+		input string
+		want  int
+	}{
+		{
+			input: "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+			want:  19,
+		},
+		{
+			input: "bvwbjplbgvbhsrlpgdmjqwftvncz",
+			want:  23,
+		},
+		{
+			input: "nppdvjthqldpwncqszvftbrmjlhg",
+			want:  23,
+		},
+		{
+			input: "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+			want:  29,
+		},
+		{
+			input: "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+			want:  26,
+		},
+	}
+
+	for i, tc := range tests {
+		got := StartOfMessage(tc.input)
 		if got != tc.want {
 			t.Fatalf("[%d]: expected: %v, got: %v", i, tc.want, got)
 		}
--- 2022/6/part2.go
+++ 2022/6/part2.go
@@ -1,6 +1,9 @@
 package main
 
+import "fmt"
+
 type Part2Solver struct {
+	result int
 }
 
 func NewPart2Solver() *Part2Solver {
@@ -8,8 +11,34 @@ func NewPart2Solver() *Part2Solver {
 }
 
 func (s *Part2Solver) Line(l string) {
+	s.result = StartOfMessage(l)
 }
 
 func (s *Part2Solver) End() string {
-	return ""
+	return fmt.Sprintf("%d", s.result)
+}
+
+func StartOfMessage(i string) int {
+
+	uniqLen := 14
+
+	b := make([]rune, 0)
+
+	for k, v := range i {
+		b = append(b, v)
+		if len(b) > uniqLen {
+			b = b[1:]
+		}
+
+		uniq := make(map[rune]int)
+		for _, r := range b {
+			uniq[r] = 1
+		}
+
+		if len(uniq) == uniqLen {
+			return k + 1
+		}
+	}
+
+	return 0
 }
