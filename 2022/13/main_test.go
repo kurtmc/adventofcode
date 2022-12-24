2d8f9a82f95d1733fb55a1072b8bf4c40d992963
Kurt McAlpine <kurt@linux.com>
2022-12-25 09:18:22 +1300

    Part 1

--- 2022/13/main_test.go
+++ 2022/13/main_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestExpandGrid(t *testing.T) {
+func TestConvert(t *testing.T) {
 	tests := []struct {
 		input struct {
 			packet string
@@ -32,6 +32,139 @@ func TestExpandGrid(t *testing.T) {
 
 	for i, tc := range tests {
 		got := convert(tc.input.packet)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("[%d]: expected: %v, got: %v", i, tc.want, got)
+		}
+	}
+}
+
+func TestCompare(t *testing.T) {
+	tests := []struct {
+		input struct {
+			a, b []interface{}
+		}
+		want int
+	}{
+		{
+			input: struct {
+				a, b []interface{}
+			}{
+				a: []interface{}{float64(1), float64(1), float64(3), float64(1), float64(1)},
+				b: []interface{}{float64(1), float64(1), float64(3), float64(1), float64(1)},
+			},
+			want: 0,
+		},
+		{
+			input: struct {
+				a, b []interface{}
+			}{
+				a: []interface{}{float64(1), float64(1), float64(3), float64(1), float64(1)},
+				b: []interface{}{float64(1), float64(1), float64(5), float64(1), float64(1)},
+			},
+			want: -1,
+		},
+		{
+			input: struct {
+				a, b []interface{}
+			}{
+				a: []interface{}{[]interface{}{float64(1)}, []interface{}{float64(2), float64(3), float64(4)}},
+				b: []interface{}{[]interface{}{float64(1)}, float64(4)},
+			},
+			want: -1,
+		},
+	}
+
+	for i, tc := range tests {
+		got := compare(tc.input.a, tc.input.b)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("[%d]: expected: %v, got: %v", i, tc.want, got)
+		}
+	}
+}
+
+func TestCompare2(t *testing.T) {
+	tests := []struct {
+		input struct {
+			a, b string
+		}
+		want int
+	}{
+		{
+			input: struct {
+				a, b string
+			}{
+				a: "[1,1,3,1,1]",
+				b: "[1,1,5,1,1]",
+			},
+			want: -1,
+		},
+		{
+			input: struct {
+				a, b string
+			}{
+				a: "[[1],[2,3,4]]",
+				b: "[[1],4]",
+			},
+			want: -1,
+		},
+		{
+			input: struct {
+				a, b string
+			}{
+				a: "[9]",
+				b: "[[8,7,6]]",
+			},
+			want: 1,
+		},
+		{
+			input: struct {
+				a, b string
+			}{
+				a: "[[4,4],4,4]",
+				b: "[[4,4],4,4,4]",
+			},
+			want: -1,
+		},
+		{
+			input: struct {
+				a, b string
+			}{
+				a: "[7,7,7,7]",
+				b: "[7,7,7]",
+			},
+			want: 1,
+		},
+		{
+			input: struct {
+				a, b string
+			}{
+				a: "[]",
+				b: "[3]",
+			},
+			want: -1,
+		},
+		{
+			input: struct {
+				a, b string
+			}{
+				a: "[[[]]]",
+				b: "[[]]",
+			},
+			want: 1,
+		},
+		{
+			input: struct {
+				a, b string
+			}{
+				a: "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+				b: "[1,[2,[3,[4,[5,6,0]]]],8,9]",
+			},
+			want: 1,
+		},
+	}
+
+	for i, tc := range tests {
+		got := compare(convert(tc.input.a), convert(tc.input.b))
 		if !reflect.DeepEqual(tc.want, got) {
 			t.Fatalf("[%d]: expected: %v, got: %v", i, tc.want, got)
 		}
--- 2022/13/part1.go
+++ 2022/13/part1.go
@@ -1,23 +1,101 @@
 package main
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
 
 type Part1Solver struct {
+	a, b  string
+	index int
+	sum   int
 }
 
 func NewPart1Solver() *Part1Solver {
-	return &Part1Solver{}
+	return &Part1Solver{index: 1}
 }
 
 func (s *Part1Solver) Line(l string) {
+
+	if l == "" {
+		cmp := compare(convert(s.a), convert(s.b))
+		fmt.Println(cmp)
+		if cmp == 0 || cmp == -1 {
+			s.sum = s.sum + s.index
+		}
+
+		s.index++
+		s.a = ""
+		s.b = ""
+	} else {
+		if s.a == "" {
+			s.a = l
+		} else {
+			s.b = l
+		}
+	}
 }
 
 func (s *Part1Solver) End() string {
-	return ""
+	return fmt.Sprintf("%d", s.sum)
 }
 
 func convert(input string) []interface{} {
 	result := make([]interface{}, 0)
 	json.Unmarshal([]byte(input), &result)
 	return result
+}
+
+func compare(a, b []interface{}) int {
+	fmt.Printf("compare(%v, %v)\n", a, b)
+
+	lenA := len(a)
+	lenB := len(b)
+
+	min := lenA
+	if lenB < min {
+		min = lenB
+	}
+
+	for i := 0; i < min; i++ {
+		k := i
+		v := a[i]
+		if reflect.TypeOf(v) == reflect.TypeOf(b[k]) {
+			if reflect.TypeOf(v).Kind() == reflect.Float64 {
+				if v.(float64) < b[k].(float64) {
+					return -1
+				} else if v.(float64) > b[k].(float64) {
+					return 1
+				}
+			} else {
+				cmp := compare(v.([]interface{}), b[k].([]interface{}))
+				if cmp != 0 {
+					return cmp
+				}
+			}
+		} else {
+			var newA, newB []interface{}
+			if reflect.TypeOf(v).Kind() == reflect.Float64 {
+				newA = []interface{}{v}
+				newB = b[k].([]interface{})
+			} else {
+				newA = v.([]interface{})
+				newB = []interface{}{b[k]}
+			}
+			cmp := compare(newA, newB)
+			if cmp != 0 {
+				return cmp
+			}
+		}
+
+	}
+
+	if lenA < lenB {
+		return -1
+	}
+	if lenA > lenB {
+		return 1
+	}
+	return 0
 }
